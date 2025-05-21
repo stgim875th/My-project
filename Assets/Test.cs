53f1db62ab69a6c00d8fb59625b1366a67df560c
Seongtae Gim <stgim@virnect.com>
2025-05-21 11:28:56 +0900

    Update .gitignore and remove ignored files from tracking

---
+++ Assets/Test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hello, World를 Console 창에 출력한다.
+        Debug.Log("Hello, World");
+    }
+}
---
+++ Assets/Test1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //변수 선언하기(74p ~ 75p)
+        //코드 2-3 변수 사용하기
+        int age;
+        age = 30;
+        Debug.Log(age);
+    }
+}
